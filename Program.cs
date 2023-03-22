52a9acbc69fb36134beb1034b696267012ae2d4f
Oleg <slobodchikov_1990@mail.ru>
2023-03-22 20:05:54 +0300

    solved task23

--- Program.cs
+++ Program.cs
@@ -4,3 +4,9 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
 
+int a;
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out a);
+for(int i=1; i<=a;i++){
+    Console.Write($"{i*i*i}, ");
+}
